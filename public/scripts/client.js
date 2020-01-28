b2383f0a63a37d42299127e1f8dbfeb48ede94fb
Hayder-Suwaed <haiderhachim87@yahoo.com>
2020-01-28 06:58:29 +0000

    implemnet function to calculte time of the tweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,23 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// Convert Numbers to display the tweet time.
+const date = (date) => {
+  const milliseconds = Math.round(Date.now() - date);
+  const millisecondsInDay = 1000 * 60 * 60 * 24;
+  const millisecondsInHours = 1000 * 60 * 60;
+  const millisecondsInMinutes = 1000 * 60;
+  if (milliseconds < millisecondsInDay && milliseconds > millisecondsInHours) {
+    return `${Math.round(milliseconds / millisecondsInHours)} hours ago`;
+  } else if (
+    milliseconds < millisecondsInHours &&
+    milliseconds > millisecondsInMinutes
+    
+  ) {
+    return `${Math.round(milliseconds / millisecondsInMinutes)} minutes ago`;
+  } else if (milliseconds < millisecondsInMinutes) {
+    return `${Math.round(milliseconds / 1000)} seconds ago`;
+  } else {
+    return `${Math.round(milliseconds / millisecondsInDay)} days ago`;
+  }
+};
