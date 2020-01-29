fc9fb632df5f98a948af3262a0d129c0d8a3beee
Hayder-Suwaed <haiderhachim87@yahoo.com>
2020-01-29 20:59:45 +0000

    create tweet element function and pass the handlers ti it

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -29,16 +29,16 @@ const getDate = function(time) {
 //Creating Tweet Element
 const createTweetElement = function(data) {
   return `
-  
+
   <article class='tweet'>
     <div class="tweet-header">
       <img src="${data.user.avatars}">
       <h3 class="tweet-name">${data.user.name}</h3>
       <h3 class="handle">${data.user.handle}</h3>
     </div>
-  
+
     <div class="text">${escape(data.content.text)}</div><br />
-  
+
             <span class="tweet-time">${getDate(data.created_at)}</span>
     <div class="tweet-icons">
       <i class="fas fa-flag"></i>
@@ -49,98 +49,3 @@ const createTweetElement = function(data) {
    `;
 };
 
-// To escape unsafe characters
-const escape = function(str) {
-  let div = document.createElement("div");
-  div.appendChild(document.createTextNode(str));
-  return div.innerHTML;
-};
-
-// Rendering the Tweets
-function renderTweets(tweets) {
-  $("#tweet-container").empty();
-  tweets.forEach((tweet) => {
-    const newtweet = createTweetElement(tweet);
-    $("#tweet-container").prepend(newtweet);
-  });
-}
-
-// Load the Tweets
-const loadTweets = function() {
-  $.ajax("/tweets/", {
-    method: "GET"
-  }).then((tweets) => {
-    renderTweets(tweets);
-  });
-};
-
-$(document).ready(function() {
-  loadTweets();
-
-  $("#form").on("submit", function(event) {
-    event.preventDefault();
-    loadTweets();
-    const tweetForm = $(this).serialize();
-
-    const inputLength = $("#text").val().length;
-
-    if (inputLength > 140) {
-      $(".alert2").css("display", "block");
-    } else if (inputLength === 0) {
-      $(".alert").css("display", "block");
-    } else {
-      $.ajax({
-        type: "POST",
-        url: "/tweets",
-        //returns "text=string"
-        data: tweetForm,
-        success: function(form) {
-          console.log("POST request completed correctly.", form);
-          loadTweets();
-          $("#text").val("");
-        }
-      });
-    }
-    loadTweets();
-  });
-  $(".arrow").on("click", function() {
-    $(".toggle").toggle();
-    $("#text").focus();
-  });
-  $(".closebtn").on("click", function() {
-    $(".alert").css("display", "none");
-  });
-  $(".closebtn1").on("click", function() {
-    $(".alert2").css("display", "none");
-  });
-});
-
-//Hides error message, new tweet form and scroll button on page load
-$(document).ready(() => {
-  $(".alert").hide();
-  // $('.new-tweet').hide();
-  $(".float-button").hide();
-});
-//Reveals scroll-to-top-of-page button when user scrolls down page
-$(document).ready(() => {
-  $(window).scroll(() => {
-    if ($(window).scrollTop() > 400) {
-      $(".float-button:hidden")
-        .show()
-        .animate({ opacity: 0.9 }, 1500);
-    } else if ($(window).scrollTop() <= 400) {
-      $(".float-button:visible").fadeOut(1000);
-    }
-  });
-});
-
-//Scrolls window to top of page when button is clicked
-$(document).ready(() => {
-  $(".float-button").click(() => {
-    if ($(window).width() < 1024) {
-      $("html, body").animate({ scrollTop: 400 }, "slow");
-    } else {
-      $("html, body").animate({ scrollTop: 0 }, "slow");
-    }
-  });
-});
