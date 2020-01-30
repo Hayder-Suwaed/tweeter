bd85162ab25db9af59eb25336f904294512765c5
Hayder-Suwaed <haiderhachim87@yahoo.com>
2020-01-30 22:23:35 +0000

    add screensots to README.md

--- server/routes/tweets.js
+++ server/routes/tweets.js
@@ -1,12 +1,11 @@
 "use strict";
 
-const userHelper    = require("../lib/util/user-helper")
+const userHelper = require("../lib/util/user-helper");
 
-const express       = require('express');
-const tweetsRoutes  = express.Router();
-
+const express = require("express");
+const tweetsRoutes = express.Router();
+//DateHelper function
 module.exports = function(DataHelpers) {
-
   tweetsRoutes.get("/", function(req, res) {
     DataHelpers.getTweets((err, tweets) => {
       if (err) {
@@ -16,22 +15,23 @@ module.exports = function(DataHelpers) {
       }
     });
   });
-
   tweetsRoutes.post("/", function(req, res) {
     if (!req.body.text) {
-      res.status(400).json({ error: 'invalid request: no data in POST body'});
+      res.status(400).json({ error: "invalid request: no data in POST body" });
       return;
-    }
+    } 
 
-    const user = req.body.user ? req.body.user : userHelper.generateRandomUser();
+    const user = req.body.user
+      ? req.body.user
+      : userHelper.generateRandomUser();
     const tweet = {
       user: user,
       content: {
         text: req.body.text
       },
       created_at: Date.now()
-    };
-
+    }; 
+    
     DataHelpers.saveTweet(tweet, (err) => {
       if (err) {
         res.status(500).json({ error: err.message });
@@ -42,5 +42,4 @@ module.exports = function(DataHelpers) {
   });
 
   return tweetsRoutes;
-
-}
+};
